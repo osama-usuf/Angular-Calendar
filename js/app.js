13260791fb967acaca5cf18ad29bbe2b361d1423
Osama Yousuf <35128083+osama-usuf@users.noreply.github.com>
2020-06-14 18:02:22 +0500

    Added Day View Logic

--- js/app.js
+++ js/app.js
@@ -27,16 +27,63 @@ function CalendarController(ApiService, $mdDialog)
 	calendar.totalDays = 7;
 	// Helper functions for calendar functionality
 
-	calendar.days = getTotalDays(curDate.getMonth(), calendar.curYear);
+	calendar.viewDays = getViewDays(curDate.getMonth(), calendar.curYear);
+
+	//calendar.viewDays = getViewDays(2, 2020);
 
 	function getTotalDays(month, year)
 	{
 		// Given month & year, outputs the total number of days
 		// in that particular month and the offset for the first day
 		// (month, year) -> (numDays, firstDay)
-		var firstDay = new Date(year, month, 1).toLocaleDateString('default', { weekday: 'long' });
-		var numDays = new Date(year, month, 0).getDate();
+		var firstDay = new Date(year, month, 1).getDay();
+		var numDays = 32 - new Date(year, month, 32).getDate();
 		return {'numDays': numDays, 'firstDay': firstDay}
+	}
+
+
+	function getViewDays(month, year)
+	{
+		var totalDays_curView  = getTotalDays(month, year); 
+		var nextMonth = month; var prevMonth = month;
+		var nextYear =   year; var prevYear  = year;
+
+	 	// Edge cases for next/prev years 
+		if 		(month ==  0) 	{ prevYear -= 1; }
+		else if (month == 11)	{ nextYear += 1; }
+
+		nextMonth = (nextMonth + 1) % 12;
+		prevMonth = (((prevMonth - 1) % 12) + 12) % 12;
+
+
+		var totalDays_prevView = getTotalDays(prevMonth, prevYear);
+		var totalDays_nextView = getTotalDays(nextMonth, nextYear);
+
+		// Figure out initial padding required
+
+		// Create the final view array
+
+		var initPadding = totalDays_curView['firstDay'];
+		var arrSize = initPadding + totalDays_curView['numDays'];
+		var daysArr = [];
+
+		for (var i=0; i < initPadding; i++)
+		{
+			daysArr.push({'value':totalDays_prevView['numDays']- (initPadding-i) + 1,
+						  'disabled': true});
+		}
+		for (var i=0; i < totalDays_curView['numDays']; i++)
+		{
+			daysArr.push({'value':totalDays_curView['numDays'] - (totalDays_curView['numDays'] - i) + 1,
+						  'disabled': false});
+		}
+		var i = 1;
+		while (daysArr.length % 7 != 0)
+		{
+			daysArr.push({'value':i++,
+						  'disabled': true});
+		}
+		console.log(daysArr);
 	}
 
 	calendar.getDayName = function(day)
